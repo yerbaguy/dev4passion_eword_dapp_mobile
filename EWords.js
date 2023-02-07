bf6f84bceeb5a27c8066ca261ae3bf0566b05043
System Administrator <root@bartoszs-MacBook-Pro.local>
2023-02-07 01:44:31 +0100

    Added to dev4passion_eword_dapp_backup_15_12_2022_connects_to_metamask

--- EWords.js
+++ EWords.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
-const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -10,11 +8,7 @@ import {
   useColorScheme,
   View,
   TouchableOpacity,
-  Button,
-  onPress
 } from 'react-native';
-
-import Web3 from 'web3';
 
 import {Colors, Header} from 'react-native/Libraries/NewAppScreen';
 
@@ -25,26 +19,6 @@ import {
   withWalletConnect,
 } from '@walletconnect/react-native-dapp';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-//import { EWordContract } from './EWord';
-import  EWordContractt  from './utils/EWordContract.json';
-
-import { UInt256, U256 } from 'uint256';
-import { parse } from 'dotenv';
-
-const ewordAddress = "0x047F65031c8aBf370FDBfEf667B0b1fd702F09Ef"
-
-const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-var Contract = require('web3-eth-contract');
-
-//Using HTTPS
-////////const web3 = createAlchemyWeb3("https://eth-mainnet.g.alchemy.com/1NkuHJk9fySa1xwgPZ21rwqkGJbh_9Cm");
-
-// const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
-// const web3 = createAlchemyWeb3(alchemyKey); 
-
-//////console.warn(web3);
-
-//console.warn(web3);
 
 const shortenAddress = (address: string) => {
   return `${address.slice(0, 6)}...${address.slice(
@@ -85,66 +59,6 @@ const Section: React.FC<{
 
     const connector = useWalletConnect();
 
-    const [data, setData] = React.useState([]);
-    const [ewords_count, setEwordsCount] = React.useState(1);
-
-    Contract.setProvider('wss://eth-goerli.g.alchemy.com/v2/1NkuHJk9fySa1xwgPZ21rwqkGJbh_9Cm');
-
-    const contractt = new Contract(EWordContractt.abi, ewordAddress);
-
-    // const web3 = React.useMemo(
-    //   () => new Web3('https://alfajores-forno.celo-testnet.org')
-    // );
-
-
-    useEffect(()=>{
-
-      // console.log("web3", web3);
-
-      Contract.setProvider('wss://eth-goerli.g.alchemy.com/v2/1NkuHJk9fySa1xwgPZ21rwqkGJbh_9Cm');
-
-      const contractt = new Contract(EWordContractt.abi, ewordAddress);
-       
-      //const account = contract.methods.accounts
-       // console.log("account", account)
-
-      console.log("contract", contractt);
-
-
-      var result = contractt.methods.getEngWordPlWord(4).call((error, result) => {
-        console.log(result);
-
-        const result_length = result.length
-    });
-
-  //   var result = contractt.methods.getEngWordPlWord(ewords_count).call((error, result) => {
-  //     console.log(result);
-
-  //     const result_length = result.length
-  // });
-
-
-    var result_ewords = contractt.methods.getEWords().call((error, result) => {
-
-        console.log("ewords_result", result.length);
-        setEwordsCount(result.length);
-        // console.log("ewords_lenght", ewords_count);
-    })
-
-          //  const result =  contract.methods.getEngWordPlWord(1)
-         /////// const result =  contract.methods.getEngWordPlWord(1)
-             // const result1 = contract[getEngWordPlWord](1)
-
-           ///////console.log("result", result);
-            //console.log("result1", result1);
-
-      // loadEWordContract();
-
-    // },[],[data])
-  },[])
-
-    // const connector = useWalletConnect();
-
     const connectWallet = React.useCallback(() => {
       return connector.connect();
     }, [connector]);
@@ -152,92 +66,6 @@ const Section: React.FC<{
     const killSession = React.useCallback(() => {
       return connector.killSession();
     }, [connector]);
-
-
-
-    // function getRandomInt() {
-
-       const submitEWord = () => {
-     // submitEWord() {
-
-
-
-        console.log("lkajsdlfk")
-        getRandomInt()
-        console.log("ewords_lenght", ewords_count);
-
-        var result_ewords = contractt.methods.getEWords().call((error, result) => {
-
-          console.log("ewords_result", result.length);
-          setEwordsCount(result.length);
-          // console.log("ewords_lenght", ewords_count);
-      })
-
-           
-
-        //  const word_number = ewords_count;
-        // const word_number = Number.parseInt(ewords_count) * 1e18;
-      
-      //////  var word_number = Number.parseInt(ewords_count);
-      
-        // var wordnumber: UInt256 = ewords_count; wrong
-         var wordnumber = 2;
-        //var wordnumber = ewords_count;
-      
-       ///// console.log("word_number", word_number);
-        
-        //  const wordnumber = U256(word_number);
-        //  console.log("wordnumber", wordnumber);
-
-        var result = contractt.methods.getEngWordPlWord(wordnumber).call((error, result) => {
-
-          // var BigNumber = require('bignumber.js');
-
-          
-          // var big_number = BigNumber(result);
-
-             
-
-            
-         // console.log(big_number);
-
-
-          console.log(result);
-    
-         // const result_length = result.length
-      });
-
-      }
-
-      const getRandomInt = () => {
-
-
-
-      // min = Math.ceil(min);
-      // max = Math.floor(max);
-
-      const data = 10;
-
-    const min = Math.ceil(1);
-      // max = Math.floor(data.length);
-  
-  ///////    const max = Math.floor(data);
-
-  // const max = Math.floor(5);
-  const max = Math.floor(ewords_count);
-      // return Math.floor(Math.random() * (max - min) + min);
-      const dataa =  Math.floor(Math.random() * (max - min) + min);
-
-      console.log("data", dataa);
-
-      setEwordsCount(dataa);
-      console.log("setEWordsCount", ewords_count);
-  
-  
-  //////    fetchEngWord(dataa);
-  
-    }
-
 
     return (
         <View>
@@ -261,10 +89,6 @@ const Section: React.FC<{
               </>
             )}
           </Section>
-
-
-          {/* <Button title="submit" onPress={()=>{submitEWord}}/> */}
-          <Button title="submit" onPress={submitEWord}/>
 
         </View>
     )
@@ -315,27 +139,6 @@ const Section: React.FC<{
       fontWeight: '600',
     },
   });
-export const loadEWordContract = async() => {
-
-  const ewords = await EWordContract.methods.getEWords().call();
-  setData(ewords);
-  console.warn(ewords);
- 
-  // const ewordss =  JSON.parse(ewords)
-    // setData(ewordss)
-    // console.warn(ewordss.getEngWordPlWord(2));
-
-   // const result = JSON.parse(ewordss);
-    // setData(result);
-    // console.warn(result);
-
-
-
-   // console.debug(ewordss)
-   // console.warn(data.getEWords());
-   // console.warn(ewordss);
-  return ewords;
-};
 
   ////export default EWords;
 
